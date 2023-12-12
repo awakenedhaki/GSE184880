dad4c753a9bbbeee433d65ee4d0b3931262d4a44
awakenedhaki <rodrigo.vallejosch@gmail.com>
2023-12-11 19:06:30 -0800

    Remove file sample accession number and metadata from filename

--- scripts/extract.sh
+++ scripts/extract.sh
@@ -36,12 +36,18 @@ ls $host_dir \
 # Loop over each file that matches the pattern in the specified directory
 find "$target_directory" -maxdepth 1 -type f -name "GSM*.gz" \
   | while read -r file; do
-      # Extract the sample accession nunber from the file name
-      sample_accession_number=$(basename "$file" | grep -o 'GSM[0-9]*')
+      # Extract the sample accession number
+      sample_accession_number=$(basename $file | grep -o 'GSM[0-9]*')
   
+      # Remove the specified pattern from the filename
+      new_name=$(echo $file | sed -E "s/GSM[0-9]*_(Cancer|Norm)[0-9]+\.//")
+      
+      # Change name of current file to the new name
+      mv $file $new_name
+      
       # Create a directory for this pattern if it doesn't exist
       mkdir -p "${target_directory}/${sample_accession_number}"
-  
+      
       # Move files with this pattern to the corresponding directory
-      mv "$file" "${target_directory}/${sample_accession_number}/"
+      mv $new_name "${target_directory}/${sample_accession_number}"
   done
