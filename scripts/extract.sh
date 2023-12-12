ca6332369a84fc48a8246a543610fc83ede8df55
awakenedhaki <rodrigo.vallejosch@gmail.com>
2023-12-11 18:35:17 -0800

    Group files in subdirectories by sample accession number

--- scripts/extract.sh
+++ scripts/extract.sh
@@ -33,3 +33,15 @@ ls $host_dir \
   | xargs -I {} mv -v "${host_dir}{}" $target_directory 2>&1 \
   | tee $log_name
 
+# Loop over each file that matches the pattern in the specified directory
+find "$target_directory" -maxdepth 1 -type f -name "GSM*.gz" \
+  | while read -r file; do
+      # Extract the sample accession nunber from the file name
+      sample_accession_number=$(basename "$file" | grep -o 'GSM[0-9]*')
+  
+      # Create a directory for this pattern if it doesn't exist
+      mkdir -p "${target_directory}/${sample_accession_number}"
+  
+      # Move files with this pattern to the corresponding directory
+      mv "$file" "${target_directory}/${sample_accession_number}/"
+  done
