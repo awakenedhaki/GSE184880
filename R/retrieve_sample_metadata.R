06bf17405ddd054751bbee24c82166622f62a745
awakenedhaki <rodrigo.vallejosch@gmail.com>
2023-12-12 14:05:07 -0800

    Merge GSE sample metadata with supplementary table 1 from article

--- R/retrieve_sample_metadata.R
+++ R/retrieve_sample_metadata.R
@@ -1,10 +1,13 @@
 # Load libraries
 library(here)
+library(dplyr)
 library(readr)
+library(readxl)
 library(GEOquery)
 
 # Get data/raw/ absolute path
-RAW_DATA <- here("data", "raw")
+DATA <- here("data")
+RAW_DATA <- here(DATA, "raw")
 
 # Load sample metadata using GEOquery
 gse <- getGEO("GSE184880", GSEMatrix = TRUE)
@@ -27,9 +30,29 @@ pruned_sample_metadata <- sample_metadata[columns]
 colnames(pruned_sample_metadata) <- names(columns)
 
 # Remove "age: " prefix
-pruned_sample_metadata$age <- gsub("age: ", "", pruned_sample_metadata$age)
+pruned_sample_metadata$age <- 
+  as.double(gsub("age: ", "", pruned_sample_metadata$age))
+
+# Load supplementary table 1 from article
+supplementary_table_1 <- read_excel(here(DATA, "supplementary_table_1.xlsx"), skip = 1)
+
+colnames(supplementary_table_1) <- 
+  gsub(x = tolower(colnames(supplementary_table_1)), 
+       pattern = " ", 
+       replacement = "_")
+
+supplementary_table_1 <- supplementary_table_1[,colnames(supplementary_table_1) != "sample"]
+
+# Merge GSE184880 sample metadata with supplementary table 1
+merged_metadata <- left_join(
+  pruned_sample_metadata, 
+  supplementary_table_1,
+  by = join_by(age == age,
+               stage == tumor_stage,
+               pathology == pathology)
+)
 
 # Save sample metadata
-write_csv(pruned_sample_metadata, here("data", "sample_metadata.csv"))
+write_csv(merged_metadata, here(DATA, "sample_metadata.csv"))
 
          
